f330fe234c8d6b23d14136f47612aff0cf66f157
Laurence Tuchin <laurencetuchin@gmail.com>
2022-02-23 11:53:43 +1100

    fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,26 @@
 module.exports = {
-	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	content: [
+		"./index.html",
+		"./src/**/*.{vue,js,ts,jsx,tsx}",
+		"./node_modules/flowbite/**/*.js",
+	],
 	theme: {
 		extend: {},
 	},
 	plugins: [],
+
+	// Filenames to scan for classes
+	content: [
+		"./src/**/*.html",
+		"./src/**/*.js",
+		"./src/**/*.jsx",
+		"./src/**/*.ts",
+		"./src/**/*.tsx",
+		"./public/index.html",
+	],
+	// Options passed to PurgeCSS
+	options: {
+		// Whitelist specific selectors by name
+		// whitelist: [],
+	},
 };
